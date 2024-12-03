4599aab0d950d338f2b94518db9e1a8012726c69
LAPTOP-7A0U20DQ\ashindas.c <ashindas.c@zcocorporation.com>
2024-12-03 12:25:14 +0530

    home screen meditation list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,18 @@
-import { Text } from "react-native"
-export default function HomeScreen()
-{
-    return <Text>Home Screen</Text>
+import { FlatList, Text, View } from "react-native"
+import { meditations } from "../data"
+import React from "react";
+import { Meditation } from "../types";
+import { MeditationListItem } from "../components/MeditationListItem";
+
+
+
+
+export default function HomeScreen() {
+    return (
+        <FlatList 
+        className="bg-white"
+        contentContainerClassName="gap-5 p-3"
+        data={meditations} renderItem={({item})=><MeditationListItem meditation={item}/>}/>
+    )
+
 }
